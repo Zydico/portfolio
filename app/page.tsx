c4fdd420e7d69afdcf69a60339034415c4ebff84
Zydico <matthewhwang95@gmail.com>
2025-07-27 20:37:06 -0700

    Changed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { Roboto } from "next/font/google";
 
+const robotoFont = Roboto({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-roboto",
+});
 
 import Hero from "@/components/Hero/Hero";
 import Navigation from "@/components/Navigation/Navigation";
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -10,22 +10,19 @@ const Navigation = () => {
 
     return (
         <section>
-            <nav className="flex items-center text-white bg-dark-background h-12" aria-label="top-menu">
-                <button className={`menu-button h-6 w-8 left-3 md:invisible absolute cursor-pointer z-3
+            <nav className="flex items-center justify-center md:justify-end md:pr-30 text-white h-12 md:h-14" aria-label="top-menu">
+                <ul className="flex gap-6 text-xl pb-1">
+                    <li id="test">Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+                {/* <button className={`menu-button h-6 w-8 left-3 md:invisible absolute cursor-pointer z-3
                                 flex flex-col justify-between ${activeMenu && "active"}`} onClick={hamburgerClick} >
                     <span></span>
                     <span></span>
                     <span></span>
-                </button>
-            </nav>
-            <nav className={`mobile-menu flex z-2 h-screen w-screen absolute top-0 left-0
-                            md:invisible ${activeMenu && "active"}`} aria-label="mobile-menu">   
-                <div className="text-white bg-darkish-background h-full w-60 shadow-lg shadow-white">
-
-                </div>
-                {
-                    activeMenu && <div className="h-full grow opacity 0" ></div> 
-                }          
+                </button> */}
             </nav>
         </section>
     )
