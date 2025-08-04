0ccb89576831930be1cb4dc656e564ad35ae53b0
Zydico <matthewhwang95@gmail.com>
2025-08-04 04:02:25 -0700

    Added proximity-opacity for constellation lines

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,15 +6,17 @@ import starsData from './star_data.json';
 import lineData from './line_data.json';
 
 const Hero = (props) => {
-    const [size, setSize] = useState([0, 0]);
+    const animationFrameId = useRef(0);
     const starCanvasRef = useRef<HTMLCanvasElement>(null);
     const starCanvasContextRef = useRef<CanvasRenderingContext2D>(null);
     const lineCanvasRef = useRef<HTMLCanvasElement>(null);
     const lineCanvasContextRef = useRef<CanvasRenderingContext2D>(null);
     const stars = new Map<string, Star>(Object.entries(starsData));
     const lines = new Map<string, string[][]>(Object.entries(lineData));
+    const starsInALine: string[] = [];
     const mousePosition: MousePosition = { x: 0, y: 0 }
     let scale: number = 1.8; // width scaling
+    let rotation: number = 0;
 
     interface MousePosition {
         x: number,
@@ -28,6 +30,10 @@ const Hero = (props) => {
     }
 
     useEffect(() => {
+        const animate = () => {
+            drawLines();
+            animationFrameId.current = requestAnimationFrame(animate);
+        }
         const handleMouseMove = (event: MouseEvent) => {
             mousePosition.x = event.clientX;
             mousePosition.y = event.clientY - props.navBarRef.current.offsetHeight;
@@ -38,12 +44,24 @@ const Hero = (props) => {
             location.reload();
         }
         window.addEventListener('resize', resize);
+
+        for (let [key, value] of lines) {
+            for (let line of value) {
+                for (let i = 0; i < line.length-1; i++) {
+                    const star = line[i];
+                    if (!starsInALine.includes(star)) {
+                        starsInALine.push(star);
+                    }
+                }
+            }       
+        }   
+
         const setCanvas = () => {
             const starCanvas: HTMLCanvasElement | null = starCanvasRef.current;
             const lineCanvas: HTMLCanvasElement | null = lineCanvasRef.current;
             if (starCanvas && lineCanvas) {
-                let width = starCanvas.getBoundingClientRect().width;
-                let height = starCanvas.getBoundingClientRect().height;
+                const width = starCanvas.getBoundingClientRect().width;
+                const height = starCanvas.getBoundingClientRect().height;
                 scale = Math.max(width, height)/1980;
                 starCanvas.width = width*2.5;
                 starCanvas.height = height*2.5;
@@ -66,10 +84,12 @@ const Hero = (props) => {
                 drawStars();
                 drawLines();
             }
+            animate();
         }
         setCanvas();
         // Cleanup
         return () => {
+            cancelAnimationFrame(animationFrameId.current);
             window.removeEventListener('resize', resize);
             window.removeEventListener('mousemove', handleMouseMove);
         }
@@ -77,15 +97,21 @@ const Hero = (props) => {
 
     const drawStars = () => {
         if (starCanvasContextRef && starCanvasContextRef.current && starCanvasRef && starCanvasRef.current) {
-            let distance = Math.max(window.innerWidth, window.innerHeight) / 1.75;
-            let ref = starCanvasContextRef.current;
+            const distance = Math.max(window.innerWidth, window.innerHeight) / 2;
+            const ref = starCanvasContextRef.current;
             ref.clearRect(-starCanvasRef.current.width/2, -starCanvasRef.current.height/2, starCanvasRef.current.width, starCanvasRef.current.height);
             ref.shadowBlur = 15; // adds glow to each star
             let circle_rad: number = 2*Math.PI;
             for (let [key, value] of stars) {
                 ref.beginPath();
+                if (value.b > 2.5) {
+                    value.b *= 0.5;
+                }
                 let brightness = Math.max(value.b, 0.4) * scale;
-                ref.arc(value.x * distance, -(value.y * distance), brightness, 0, circle_rad);
+                if (starsInALine.includes(key)) {
+                    brightness += 0.5;
+                }
+                ref.arc(value.x * distance, value.y * distance, brightness, 0, circle_rad);
                 ref.fillStyle = value.color;
                 ref.shadowColor = value.color;      
                 ref.fill();
@@ -93,48 +119,65 @@ const Hero = (props) => {
         }
     }
 
+    const getRotatedCoordinates = (x: number, y: number, angle: number): { x: number, y: number } => {
+        const sin = Math.sin(angle);
+        const cos = Math.cos(angle);
+        const rotatedX = x * cos - y * sin;
+        const rotatedY = x * sin + y * cos;
+        return { x: rotatedX, y: rotatedY };
+    }
+
     const drawLines = () => {
-        if (lineCanvasContextRef && lineCanvasContextRef.current && lineCanvasRef && lineCanvasRef.current) {
-            let ref = lineCanvasContextRef.current;
-            let distance = Math.max(window.innerWidth, window.innerHeight) / 1.75;
+        if (lineCanvasContextRef?.current && lineCanvasRef?.current && starCanvasRef?.current) {
+            const ref = lineCanvasContextRef.current;
+            ref.save();
+            const distance = Math.max(window.innerWidth, window.innerHeight) / 2;
             ref.shadowBlur = 15; // adds glow to each line
             ref.shadowColor = 'rgba(255, 255, 255, 0.5)';    
             ref.lineWidth = 1 * scale;
             ref.clearRect(-lineCanvasRef.current.width/2, -lineCanvasRef.current.height/2, lineCanvasRef.current.width, lineCanvasRef.current.height);
+            
+            const style = window.getComputedStyle(starCanvasRef.current);
+            const transform = style.transform;
+            const values = transform.match(/matrix\(([^)]+)\)/);
+            if (!values) {
+                return 0;
+            }
+            const parts = values[1].split(',').map(parseFloat);
+            const [a, b] = parts;
+            const angle = Math.atan2(b, a);     
+            const adjustedMouseX = (mousePosition.x - (lineCanvasRef.current.offsetLeft + lineCanvasRef.current.width / 2))/1.25;
+            const adjustedMouseY = (mousePosition.y - (lineCanvasRef.current.offsetTop + lineCanvasRef.current.height / 2))/1.25;       
             for (let [key, value] of lines) {
                 for (let line of value) {
                     for (let i = 0; i < line.length-1; i++) {
                         ref.beginPath();
-                        let id = line[i];
-                        let id2 = line[i+1];
-                        if (line[i].endsWith('*')) {
-                            id = line[i].slice(0, -1);
-                        }
-                        if (line[i+1].endsWith('*')) {
-                            id2 = line[i+1].slice(0, -1);
-                        }
-                        let star1 = stars.get(id);
-                        let star2 = stars.get(id2);
+                        const star1 = stars.get(line[i]);
+                        const star2 = stars.get(line[i+1]);
                         if (star1 && star2) {
-                            ref.moveTo(star1.x * distance, star1.y * distance);
-                            ref.lineTo(star2.x * distance, star2.y * distance);
-                            let center = { x: (star1.x + star2.x)*distance/2, y: (star1.y + star2.y)*distance/2 };
-                            let deltaX = (mousePosition.x - (lineCanvasRef.current.offsetLeft + lineCanvasRef.current.width / 2))/1.25 - center.x;
-                            let deltaY = (mousePosition.y - (lineCanvasRef.current.offsetTop + lineCanvasRef.current.height / 2))/1.25 - center.y;
-                            let deltaH = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));  
-                            let opacity = Math.max(1 - deltaH/200, 0);
+                            const star1Coordinates = getRotatedCoordinates(star1.x*distance, star1.y*distance, angle);
+                            const star2Coordinates = getRotatedCoordinates(star2.x*distance, star2.y*distance, angle);
+                            ref.moveTo(star1Coordinates.x, star1Coordinates.y);
+                            ref.lineTo(star2Coordinates.x, star2Coordinates.y);
+                            const centerX = (star1Coordinates.x + star2Coordinates.x)/2;
+                            const centerY = (star1Coordinates.y + star2Coordinates.y)/2;
+                            const deltaX = centerX - adjustedMouseX;
+                            const deltaY = centerY - adjustedMouseY;
+                            const deltaH = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));  
+                            const opacity = Math.max(1 - deltaH/200, 0);
                             ref.strokeStyle = `rgba(255, 255, 255, ${opacity})`; 
                             ref.stroke();
                         }
                     }
                 }       
             }   
+            ref.restore();
         }
     }
     return (
         <section className="relative overflow-hidden h-full ">
             <canvas className="absolute -top-3/4 -left-3/4 w-full h-full origin-center" ref={lineCanvasRef}></canvas>
-            <canvas className="absolute -top-3/4 -left-3/4 w-full h-full origin-center" ref={starCanvasRef}></canvas>
+            <canvas id="starCanvas" className="absolute -top-3/4 -left-3/4 w-full h-full origin-center" ref={starCanvasRef}></canvas>
             <img src="/images/foreground.svg" alt="" className="object-fill absolute bottom-0 w-full h-1/1.5" />
         </section>
     )
