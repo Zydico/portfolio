e6d6ecd705fe96e8f6a2cb9e9693dfc7b45b3162
Zydico <matthewhwang95@gmail.com>
2025-08-15 16:36:29 -0700

    About page progress

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,7 +1,62 @@
-import React, { RefObject } from 'react';
+'use client';
+import React, { RefObject, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 
 const About = (props: { aboutRef: RefObject<HTMLElement | null> }) => {
+    const skillsCanvasRef = useRef<HTMLCanvasElement>(null);
+    const skillsCanvasContextRef = useRef<CanvasRenderingContext2D>(null);
+    const animationFrameId = useRef<number>(0);
+
+    useEffect(() => {
+        const animate = () => {
+            drawSkills();
+            animationFrameId.current = requestAnimationFrame(animate);
+        }
+        const handleMouseMove = (event: MouseEvent) => {
+            // if (props.navBarRef && props.navBarRef.current) {
+            //     mousePosition.x = event.clientX;
+            //     mousePosition.y = event.clientY - props.navBarRef.current.offsetHeight;
+            //     drawLines();
+            // }
+        }
+        const setCanvas = () => {
+            const skillsCanvas: HTMLCanvasElement | null = skillsCanvasRef.current;
+            if (skillsCanvas) {
+                const width = skillsCanvas.getBoundingClientRect().width;
+                const height = skillsCanvas.getBoundingClientRect().height;
+                skillsCanvas.width = width;
+                skillsCanvas.height = height;
+                const skillsCanvasContext: CanvasRenderingContext2D | null = skillsCanvas.getContext('2d');
+                skillsCanvasContext?.translate(width/2, height/2);
+                skillsCanvasContextRef.current = skillsCanvas.getContext('2d');
+                drawSkills();
+            }
+            animate();
+        }
+        setCanvas();
+        // Cleanup
+        return () => {
+            cancelAnimationFrame(animationFrameId.current);
+            window.removeEventListener('mousemove', handleMouseMove);
+        }
+    }, [])
+
+    const drawSkills = () => {
+        if (skillsCanvasContextRef?.current && skillsCanvasRef?.current) {
+            const ref = skillsCanvasContextRef.current;
+            ref.save(); 
+            const width = skillsCanvasRef.current.width;
+            const height = skillsCanvasRef.current.height;
+            ref.clearRect(-skillsCanvasRef.current.width, -skillsCanvasRef.current.height, skillsCanvasRef.current.width, skillsCanvasRef.current.height);
+ 
+            // ref.beginPath();
+            // ref.fillStyle = 'red';
+            // ref.arc(0, 0, 40, 0, 2*Math.PI);
+            // ref.fill();
+
+            ref.restore();
+        }
+    }
         
     return (
         <section ref={props.aboutRef} id="aboutPage" className="page pt-5 pl-12 pr-12 pb-8 text-white w-full bg-[var(--color-ground)] font-(family-name:--font-body)">
@@ -35,8 +90,10 @@ const About = (props: { aboutRef: RefObject<HTMLElement | null> }) => {
                         to experiment and showcase my skills.
                     </motion.p>
                 </section>
-                <section className="md:w-40/100 w-full md:mt-0 mt-5 w-full border-1 aspect-square">
-                </section>
+                {/* <section className="md:w-40/100 w-full md:mt-0 mt-5 w-full border-1 aspect-square relative">
+                    <canvas id="skillsCanvas" className="w-full h-full origin-center" ref={skillsCanvasRef}></canvas>
+                    <img src="./images/Profile.png" alt="Profile picture" className="w-1/10 h-1/10 absolute origin-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+                </section> */}
             </div>
         </section>
     )
