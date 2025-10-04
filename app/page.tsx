da77640608fd47457341875def21202248406066
Zydico <matthewhwang95@gmail.com>
2025-10-04 12:54:06 -1000

    Next.js Routing added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 import { useRef } from "react";
-import Hero from "@/components/Hero/Hero";
-import Navigation from "@/components/Navigation/Navigation";
-import About from "@/components/About/About";
-import Projects from "@/components/Projects/Projects";
-import Contact from "@/components/Contact/Contact";
+import Navigation from "./PortfolioComponents/Navigation/Navigation";
+import Hero from "./PortfolioComponents/Hero/Hero";
+import About from "./PortfolioComponents/About/About";
+import Contact from "./PortfolioComponents/Contact/Contact";
+import Projects from "./PortfolioComponents/Projects/Projects";
 
 export default function Home() {
   const navBarRef = useRef<HTMLElement | null>(null);
@@ -14,16 +14,18 @@ export default function Home() {
   const contactRef = useRef<HTMLElement | null>(null);
 
   return (
-    <div className="flex flex-col">
-      <header className="font-menu">
-        <Navigation navBarRef={navBarRef} homeRef={homeRef} aboutRef={aboutRef} projectsRef={projectsRef} contactRef={contactRef} />
-      </header>
-      <main>
-        <Hero navBarRef={navBarRef} homeRef={homeRef} aboutRef={aboutRef} />
-        <About aboutRef={aboutRef} />
-        <Projects projectsRef={projectsRef} />
-        <Contact contactRef={contactRef} />
-      </main>
+    <div>
+      <div className="flex flex-col">
+        <header className="font-menu">
+          <Navigation navBarRef={navBarRef} homeRef={homeRef} aboutRef={aboutRef} projectsRef={projectsRef} contactRef={contactRef} />
+        </header>
+        <main>
+          <Hero navBarRef={navBarRef} homeRef={homeRef} aboutRef={aboutRef} />
+          <About aboutRef={aboutRef} />
+          <Projects projectsRef={projectsRef} />
+          <Contact contactRef={contactRef} />
+        </main>
+      </div>
     </div>
   );
 }
